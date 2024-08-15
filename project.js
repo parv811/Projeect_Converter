ef2741595464dbf4569e31ae58d3736209164cd3
PrathamVadhavana <147516297+PrathamVadhavana@users.noreply.github.com>
2024-08-15 17:46:36 +0530

    Changes wrt Date: 15/08/2024
    Changes wrt Date: 15/08/2024
    
    HTML:
    
    IDs given to following:
    (1) theme icon
    (2) menu icon
    
    Values given to following:
    (1) All convert buttons
    (2) All clear buttons
    
    CSS:
    
    (1) Assigned global variables for colors
    (2) Added CSS on label elements
    
    Important Note: Give color in form of variables nowonwards.... Read complete note at beginning of css file as the comment.
    
    JS:
    
    Created js file and added following functionality:
    
    (1) Toggle light / dark mode
    (2) Display / Hide Menu
    
    Some other minor changes also done in all three codes(No css property is affected except that is  used in js).
    No major changes were made throughout the code !

---
+++ project.js
@@ -0,0 +1,51 @@
+let main = document.getElementById('main');
+let menuButton = document.getElementById('menuButton'); //get id of menu button
+let themeButton = document.getElementById('themeButton'); //get id of theme/moon type button
+var menuButtonState = 0;  //No display menu default
+var themeButtonState = 0; //Default light mode
+let root = document.querySelector(':root'); // selects html root element
+
+//To hide and display the menu
+menuButton.addEventListener('click',()=>{
+    if(menuButtonState == 0) 
+    {
+        menu.style.display = "block";
+        menuButtonState = 1;
+    }
+    else
+    {
+        menu.style.display = "none";
+        menuButtonState = 0;
+    }
+})
+
+//To change the page theme to light or dark mode
+themeButton.addEventListener('click',()=>{
+    if(themeButtonState == 0)
+    {
+        root.style.setProperty('--fontcolorblack','white');
+        root.style.setProperty('--fontcolorwhite','black');
+        root.style.setProperty('--fontcolorbluish','grey');
+        root.style.setProperty('--bgcolorwhite','black');
+        root.style.setProperty('--bgcolorbluish','grey');
+        root.style.setProperty('--bgcolorf0f0f0','#CBC8C8');
+        root.style.setProperty('--bordercolorwhite','black');
+        root.style.setProperty('--boxshadowcolorreddish','rgba(254, 218, 241, 0.8)');
+        root.style.setProperty('--boxshadowcolorblack','rgba(255, 255, 255, 0.3)');
+        themeButtonState = 1;
+    }
+    else
+    {
+        root.style.setProperty('--fontcolorblack','black');
+        root.style.setProperty('--fontcolorwhite','white');
+        root.style.setProperty('--fontcolorbluish','lightsteelblue');
+        root.style.setProperty('--bgcolorwhite','white');
+        root.style.setProperty('--bgcolorbluish','lightsteelblue');
+        root.style.setProperty('--bgcolorf0f0f0','#f0f0f0');
+        root.style.setProperty('--bordercolorwhite','white');
+        root.style.setProperty('--boxshadowcolorreddish','rgba(255, 0, 0, 0.1)');
+        root.style.setProperty('--boxshadowcolorblack','rgba(0, 0, 0, 0.3)');
+        themeButtonState = 0
+    }
+})
+
